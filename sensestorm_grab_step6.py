11f3a0d7799771bf72b1f7267808a6a1092bcc68
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-06-18 19:32:53 +0800

    Create sensestorm_grab_step6.py

---
+++ sensestorm_grab_step6.py
@@ -0,0 +1,19 @@
+from sensestorm import Motor, UltrasonicSensor
+from time import sleep
+motor_grab = Motor("D")
+motor_move = Motor("A")
+
+angle_init = motor_move.get_angle()
+print('angle: ',angle_init)
+
+def reset():
+    angle = motor_move.get_angle() - angle_init
+    print('angle: ',angle)
+    motor_move.run_angle(0.5, angle/360)
+    ultra_sensor = UltrasonicSensor("1")
+    distance = ultra_sensor.get_distance()
+    print('distance: ',distance)
+
+motor_move.run_time(40, 0.5)
+sleep(1)
+reset()
