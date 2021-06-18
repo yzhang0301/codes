3a641628f2fb705453f3ff6b559977c06dddd4de
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-06-18 20:08:52 +0800

    Update sensestorm_grab_step8.py

--- sensestorm_grab_step8.py
+++ sensestorm_grab_step8.py
@@ -8,8 +8,6 @@ motor_move = Motor("A")
 
 global grab_flag
 grab_flag = False
-global left_flag
-left_flag = True
 
 gesture_list = ["OK","V","THUMB_UP","STOP","TICK","HEART","GRAB","FIST","FIST_PALM_SALUTE","SINGLE_HAND_HEART","FOREFINGER_UP", "SIX","PALMS_TOGETHER"]
 
@@ -36,22 +34,7 @@ def release():
     if grab_flag:
         motor_grab.run_time(-50, 0.5)
         sleep(1)
-        grab_flag = False    
-
-def control_claw_state(gesture):
-    if gesture == 3:
-        release()
-    elif gesture == 7:
-        grab()
-        
-def control_claw_location(x_center, gesture, loc):
-    if gesture == 1:
-        dist = loc - x_center
-        if dist < -100:
-            move_arm_left()
-        if dist > 100:
-            move_arm_right()
-        sleep(0.5)
+        grab_flag = False
 
 def draw_bounding_box(frame, location):
     cv2.rectangle(frame, (location[0],location[1]), (location[2],location[3]), (10, 255, 0), 2)
@@ -62,20 +45,7 @@ def draw_label(frame, location, label):
     cv2.rectangle(frame, (location[0], label_ymin-labelSize[1]-10), (location[0]+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
     cv2.putText(frame, label, (location[0], label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
 
-def move_arm_left():
-    global left_flag
-    if not left_flag:
-        motor_move.run_time(-35,0.5)
-        left_flag = True
-
-def move_arm_right():
-   global left_flag
-   if left_flag:
-        motor_move.run_time(35,0.5)
-        left_flag = False
-
 videostream = cv2.VideoCapture(0)
-previous_loc = 300
 while True:
     try:
         ret,frame = videostream.read()
@@ -86,16 +56,11 @@ while True:
         if gesture is not None:
             x_center = (location[0] + location[2])/2
             print(x_center)
-            control_claw_state(gesture)
-            control_claw_location(x_center, gesture, previous_loc)
-            previous_loc = x_center
- 
             location = [int(x) for x in location]
-
             draw_bounding_box(frame, location)
             draw_label(frame, location, str(gesture))
             time.sleep(0.1)
- 
+        
         show_image(frame)
  
         # Press 'q' to quit
