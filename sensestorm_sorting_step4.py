8e3531f550ce9ee3ae64cc076948be45b43c96b2
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-06-18 20:37:14 +0800

    Update sensestorm_sorting_step4.py

--- sensestorm_sorting_step4.py
+++ sensestorm_sorting_step4.py
@@ -25,43 +25,11 @@ convey_object = {
     'Blue': [convey_long, left_sort]
 }
 
-while True:
-    try:
-        frame = get_image()
-        show_image(frame)
-        colorlist = get_frame_color(frame)
-            
-        if colorlist is not None:
-            # convey_color(colorlist)
-            color = colorlist[0]
-            if color == 'Black':
-                continue
-            else:
-                print(color,' is found!')
-                convey_object[color][0]()
-                convey_object[color][1]()
-     
-    except:
-        print(sys.exc_info())
-        break
-
-'''
-#while True:
-frame = get_image()
-show_image(frame)
-color = get_frame_color(frame)
-print("color is: ", color)
-'''
-
-'''    
-    color = color_sensor.get_color()
-    print(color)
-    if color == 'Black':
-        pass
-    else:
-        convey_object[color][0]()
-        convey_object[color][1]()
+color = color_sensor.get_color()
+print(color)
+if color == 'Black':
+    pass
+else:
+    convey_object[color][0]()
+    convey_object[color][1]()
     sleep(1)
-'''
-
-#convey_short()
