09cefc33d9853b2b33213cce15b71bfe1d806de8
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-06-20 17:10:45 +0800

    Update introduction_I_5.5.py

--- SenseStudy/introduction/introduction_I_5.5.py
+++ SenseStudy/introduction/introduction_I_5.5.py
@@ -1,1 +1,19 @@
+train_x, train_y = load_gender()
+model = LogisticRegressor()
+model.train(train_x, train_y)
+model.show()
 
+x = 40
+pred_y = model.predict(x)
+print(pred_y)
+
+weights = model.get_weights()
+print(weights)
+k = weights[0]
+b = weights[1]
+print("k=",k)
+print("b=",b)
+
+model2 = LinearRegressor()
+model2.train(train_x, train_y)
+model2.show()
