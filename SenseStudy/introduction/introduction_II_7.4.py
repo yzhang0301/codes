217bf7adca657ef23e9cf197534c269ccefe400e
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-06-21 19:40:05 +0800

    Update introduction_II_7.4.py

--- SenseStudy/introduction/introduction_II_7.4.py
+++ SenseStudy/introduction/introduction_II_7.4.py
@@ -1,1 +1,24 @@
+from introai_chap7.cv import show_img
+from introai.image import imread
 
+trainset,testset = load_face_sample_dataset('gender')
+imgs,labels = trainset
+features = extract(imgs)
+
+model = linear_classifier()
+model.train(features,labels)
+
+imgs,labels = testset
+features = extract(imgs)
+pred = model.predict(features)
+acc = accuracy(pred,labels)
+print(acc)
+
+img_test = imgs[2]
+#img_test = imread('http://sensestudy-server/api/resource/public/accountstorage-objs/18ff97da-ead9-439d-82b5-deebcc29502a/chow.jpg')
+show_img(img_test)
+result=model.predict(extract(img_test))
+if result == 1:
+    print('男性')
+else:
+    print('女性')
