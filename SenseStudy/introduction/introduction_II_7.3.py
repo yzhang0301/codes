862f074f30f6664ebcd5533a5a1b472f2ae25920
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-06-21 19:34:42 +0800

    Update introduction_II_7.3.py

--- SenseStudy/introduction/introduction_II_7.3.py
+++ SenseStudy/introduction/introduction_II_7.3.py
@@ -1,1 +1,9 @@
+trainloader,testloader = load_deep_data(DATA_PATH)
+net = LeNet()
+train_deep_net(trainloader,testloader,net,max_epoch = 8)
 
+for i in range(20):
+    test_img = get_test_img(DATA_PATH,i)
+    show_deep_img(test_img)
+    result = deep_predict(net,test_img)
+    print(result)
