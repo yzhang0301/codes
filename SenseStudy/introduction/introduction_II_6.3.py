1b661bec7e68b5478673d16ddf7648d4ded2a958
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-06-21 20:07:41 +0800

    Update introduction_II_6.3.py

--- SenseStudy/introduction/introduction_II_6.3.py
+++ SenseStudy/introduction/introduction_II_6.3.py
@@ -1,1 +1,16 @@
+def word_freq(words):
+    freq_dict = {}
+    for word in words:
+        if word in freq_dict:
+            freq_dict[word] += 1
+        else:
+            freq_dict[word] = 1
+    for word, freq in freq_dict.items():
+        freq_dict[word] = freq / len(words)
+    return freq_dict
 
+texts, grades = load_engtext(subset='textbooks')
+text = texts[0]
+words = splitwords(text)
+freq_dict = word_freq(words)
+print(freq_dict)
