b0c58f5758e7730c15eecf9fedbbb11af4e769cf
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-06-29 17:29:48 +0800

    Update sensestorm_grasp_step3.py

--- SenseStorm/grasp/sensestorm_grasp_step3.py
+++ SenseStorm/grasp/sensestorm_grasp_step3.py
@@ -1,7 +1,4 @@
-from sensestorm import Motor, UltrasonicSensor
-from time import sleep
 motor_grab = Motor("D")
-
 ultra_sensor = UltrasonicSensor("1")
 distance = ultra_sensor.get_distance()
 print('distance: ',distance)
