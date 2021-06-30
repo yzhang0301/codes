60426602497df7dbf64c4de304bdc11c8aa7e713
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-06-30 20:49:23 +0800

    Update sensestorm_sorting_step2.py

--- SenseStorm/sorting/sensestorm_sorting_step2.py
+++ SenseStorm/sorting/sensestorm_sorting_step2.py
@@ -1,11 +1,11 @@
 motor_belt = Motor("A")
 
 def convey_short():
-    motor_belt.run_angle(0.5, 0.6)
+    motor_belt.run_angle(0.5, -0.6)
     sleep(2)
     
 def convey_long():
-    motor_belt.run_angle(0.5, 1.2)
+    motor_belt.run_angle(0.5, -1.2)
     sleep(3)
 
 convey_short()
