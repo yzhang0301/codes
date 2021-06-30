16569dbbb39e8ca191f29abc8463ac4eda2e5770
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-06-30 20:49:43 +0800

    Update sensestorm_sorting_step3.py

--- SenseStorm/sorting/sensestorm_sorting_step3.py
+++ SenseStorm/sorting/sensestorm_sorting_step3.py
@@ -2,11 +2,11 @@ motor_belt = Motor("A")
 color_sensor = ColorSensor("1")
 
 def convey_short():
-    motor_belt.run_angle(0.5, 0.6)
+    motor_belt.run_angle(0.5, -0.6)
     sleep(2)
     
 def convey_long():
-    motor_belt.run_angle(0.5, 1.2)
+    motor_belt.run_angle(0.5, -1.2)
     sleep(3)
 
 color = color_sensor.get_color()
