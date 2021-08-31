5828f98f67d6f8c2de7999245b8b7eed3dfe4fb5
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-08-31 19:44:01 +0800

    Update sensestorm_sorting_step5.py

--- SenseStorm/sorting/sensestorm_sorting_step5.py
+++ SenseStorm/sorting/sensestorm_sorting_step5.py
@@ -1,42 +1,19 @@
-from CourseHeader.API import *
+from threading import Thread
+import cv2
 
-motor_belt = Motor("A")
-motor_sort = Motor("B")
-color_sensor = ColorSensor("1")
+frame=None
+videostream = cv2.VideoCapture(0)
+def read_frame():
+    global frame
+    imW = videostream.get(cv2.CAP_PROP_FRAME_WIDTH)
+    imH = videostream.get(cv2.CAP_PROP_FRAME_HEIGHT)
+ while True:
+        ret,frame = videostream.read()
+        show_image(frame)
 
-def convey_short():
-    motor_belt.run_angle(0.5, -0.6)
-    sleep(2)
-    
-def convey_long():
-    motor_belt.run_angle(0.5, -1.2)
-    sleep(3)
+def display_video():
+    Thread(target=read_frame,args=(),daemon=True).start()
 
-def left_sort():
-    motor_sort.run_angle(0.5, -1.01)
-    sleep(2)
-
-def right_sort():
-    motor_sort.run_angle(0.5, 1)
-    sleep(2)
-
-def get_frame():
-    videostream = cv2.VideoCapture(0)
-    ret, frame = videostream.read()
-    frame = cv2.flip(frame, 1)
-    cv2.destroyAllWindows()
-    videostream.release()
-    return frame    
-
-convey_object = {
-    'Yellow': [convey_short, right_sort],
-    'Green': [convey_short, left_sort],
-    'Red': [convey_long, right_sort],
-    'Blue': [convey_long, left_sort]
-}
-
-while True:
-    frame = get_frame()
-    show_image(frame)
-    color = get_frame_color(frame)
-    print("color is: ", color)
+display_video()
+colorlist = get_frame_color(frame)
+print(colorlist)
