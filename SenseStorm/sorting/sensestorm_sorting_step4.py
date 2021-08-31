f9bc5de56a5f74de827a0f6961812e94b0eef304
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-08-31 19:43:21 +0800

    Update sensestorm_sorting_step4.py

--- SenseStorm/sorting/sensestorm_sorting_step4.py
+++ SenseStorm/sorting/sensestorm_sorting_step4.py
@@ -1,35 +1,18 @@
-motor_belt = Motor("A")
-motor_sort = Motor("B")
-color_sensor = ColorSensor("1")
-
-def convey_short():
-    motor_belt.run_angle(0.5, -0.6)
-    sleep(2)
-    
-def convey_long():
-    motor_belt.run_angle(0.5, -1.2)
-    sleep(3)
-
-def left_sort():
-    motor_sort.run_angle(0.5, -1.01)
-    sleep(2)
-
-def right_sort():
-    motor_sort.run_angle(0.5, 1)
-    sleep(2)
-
-convey_object = {
-    'Yellow': [convey_short, right_sort],
-    'Green': [convey_short, left_sort],
-    'Red': [convey_long, right_sort],
-    'Blue': [convey_long, left_sort]
-}
-
-color = color_sensor.get_color()
-print(color)
-if color == 'Black':
-    pass
-else:
-    convey_object[color][0]()
-    convey_object[color][1]()
-    sleep(1)
+while True:
+    color = color_sensor.get_color()
+    if color == "Black" or color is None:
+        continue
+    else: 
+        print(color, "is found!")
+        if color == "Yellow":
+            convey_short()
+            right_sort()
+        elif color == "Green":
+            convey_short()
+            left_sort()
+        elif color == "Red":
+            convey_long()
+            right_sort()
+        elif color == "Blue":
+            convey_long()
+            left_sort()
