98fca2f6acc617bc03523e8abaf5ec849c1b3a62
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-09-01 10:10:29 +0800

    Update sensestorm_gate_step2.py

--- SenseStorm/gate/sensestorm_gate_step2.py
+++ SenseStorm/gate/sensestorm_gate_step2.py
@@ -1,1 +1,11 @@
+motor_c = Motor("C")
+def door_control(speed,p_time):
+  if speed>0:
+    speak("Opening")
+  if speed<0:
+    speak("Closing")
+  motor_c.run_time(speed,p_time)
 
+door_control(30,0.5)
+sleep(1)
+door_control(-30,0.5)
