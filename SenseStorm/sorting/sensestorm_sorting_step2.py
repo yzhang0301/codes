f096b3839e30bf36583c1c302a31dc0bd02fd2f1
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-09-01 09:50:06 +0800

    Update sensestorm_sorting_step2.py

--- SenseStorm/sorting/sensestorm_sorting_step2.py
+++ SenseStorm/sorting/sensestorm_sorting_step2.py
@@ -11,3 +11,18 @@ def convey_long():
 convey_short()
 sleep(3)
 convey_long()
+
+
+motor_sort = Motor("B")
+
+def left_sort():
+  motor_sort.run_angle(0.5, -1.01)
+  sleep(2)
+
+def right_sort():
+  motor_sort.run_angle(0.5, 1)
+  sleep(2)
+
+left_sort()
+sleep(3)
+right_sort()
