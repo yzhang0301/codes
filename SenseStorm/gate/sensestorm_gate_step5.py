217b4f48cb70f41f10daf42f3933913e55f37ea7
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-09-01 10:17:38 +0800

    Update sensestorm_gate_step5.py

--- SenseStorm/gate/sensestorm_gate_step5.py
+++ SenseStorm/gate/sensestorm_gate_step5.py
@@ -1,1 +1,12 @@
-
+while True:
+  frame = get_image()
+  feature1 = extract_feature(frame)
+  if feature1 is None:
+    print("Unable to extract features")
+    speak("Unable to extract features")
+    sleep(2)
+  else:
+    print("Features extracted, face registered")
+    speak("Features extracted, face registered")
+    sleep(2)
+    break
