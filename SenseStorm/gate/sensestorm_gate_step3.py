54848453a2a153debcc70632fe940f4c334069b6
yzhang0301 <35363371+yzhang0301@users.noreply.github.com>
2021-09-01 10:11:16 +0800

    Update sensestorm_gate_step3.py

--- SenseStorm/gate/sensestorm_gate_step3.py
+++ SenseStorm/gate/sensestorm_gate_step3.py
@@ -1,1 +1,10 @@
+frame = get_image()
+show_image(frame)
 
+position = detect_face(frame)
+if position is None:
+  print("No face detected")
+  speak("No face detected")
+else:
+  print("Face detected")
+  speak("Face detected")
